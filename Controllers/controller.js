7405c94a9f8c5c7ff847d5853324462f7a547205
Mickabtech <Mickabtech@gmail.com>
2023-09-13 22:57:12 +0100

    updated user message

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -6,7 +6,7 @@ exports.createPerson = async (req, res) => {
     const { name } = req.body;
     const person = new User({ name });
     const savedPerson = await person.save();
-    res.status(201).json(savedPerson);
+    res.status(201).json({ message: 'User created successfully', user: savedPerson });
   } catch (error) {
     res.status(500).json({ error: 'Unable to create a person' });
   }
@@ -35,7 +35,7 @@ exports.updatePerson = async (req, res) => {
     if (!updatedPerson) {
       return res.status(404).json({ error: 'User not found' });
     }
-    res.status(200).json(updatedPerson);
+    res.status(200).json({ message: 'User updated successfully', user: updatedPerson });
   } catch (error) {
     res.status(500).json({ error: 'Unable to update a person' });
   }
